fb5b3aea0edf69a4169ab79d2908918111fc6072
lioritan <lioritan@users.noreply.github.com>
2021-04-05 20:43:10 +0300

    Mujoco fixes pn

--- curriculum_experiments/run_experiment_mujoco.py
+++ curriculum_experiments/run_experiment_mujoco.py
@@ -3,6 +3,7 @@ import os
 import pickle
 
 from rllib.algorithms.mpc import CEMShooting
+from rllib.environment import AbstractEnvironment, parse_space
 from rllib.policy import MPCPolicy
 from tqdm import tqdm
 
@@ -11,14 +12,145 @@ from curriculum_experiments.parametric_half_cheetah import HalfCheetahWrapper
 from curriculum_experiments.random_teacher import RandomTeacher
 from curriculum_experiments.task_difficulty_estimate import estimate_task_difficulties
 from hucrl.agent import MPCAgent
+from hucrl.environment.hallucination_wrapper import HallucinationWrapper
 from hucrl.model.hallucinated_model import HallucinatedModel
+
+
+class SillyEnvironmentWrapper(AbstractEnvironment):
+    def __init__(self, env, seed=None, **kwargs):
+        self.env = env
+        self.env.seed(seed)
+
+        dim_action, num_actions = parse_space(self.env.action_space)
+        dim_state, num_states = parse_space(self.env.observation_space)
+        if num_states > -1:
+            num_states += 1  # Add a terminal state.
+
+        super().__init__(
+            dim_action=dim_action,
+            dim_state=dim_state,
+            action_space=self.env.action_space,
+            observation_space=self.env.observation_space,
+            num_actions=num_actions,
+            num_states=num_states,
+            num_observations=num_states,
+        )
+        self._time = 0
+        self.metadata = self.env.metadata
+
+    def add_wrapper(self, wrapper, **kwargs):
+        """Add a wrapper for the environment."""
+        self.env = wrapper(self.env, **kwargs)
+
+        dim_action, num_actions = parse_space(self.env.action_space)
+        dim_state, num_states = parse_space(self.env.observation_space)
+        if num_states > -1:
+            num_states += 1  # Add a terminal state.
+
+        super().__init__(
+            dim_action=dim_action,
+            dim_state=dim_state,
+            action_space=self.env.action_space,
+            observation_space=self.env.observation_space,
+            num_actions=num_actions,
+            num_states=num_states,
+            num_observations=num_states,
+        )
+        self._time = 0
+
+    def pop_wrapper(self):
+        """Pop last wrapper."""
+        self.env = self.env.env
+
+        dim_action, num_actions = parse_space(self.env.action_space)
+        dim_state, num_states = parse_space(self.env.observation_space)
+        if num_states > -1:
+            num_states += 1  # Add a terminal state.
+
+        super().__init__(
+            dim_action=dim_action,
+            dim_state=dim_state,
+            action_space=self.env.action_space,
+            observation_space=self.env.observation_space,
+            num_actions=num_actions,
+            num_states=num_states,
+            num_observations=num_states,
+        )
+        self._time = 0
+
+    def step(self, action):
+        """See `AbstractEnvironment.step'."""
+        next_state, reward, done, info = self.env.step(action)
+        if self.num_states > 0 and done:  # Move to terminal state.
+            next_state = self.num_states - 1
+        self._time += 1
+        return next_state, reward, done, info
+
+    def render(self, mode="human"):
+        """See `AbstractEnvironment.render'."""
+        return self.env.render(mode)
+
+    def close(self):
+        """See `AbstractEnvironment.close'."""
+        self.env.close()
+
+    def reset(self):
+        """See `AbstractEnvironment.reset'."""
+        self._time = 0
+        return self.env.reset()
+
+    @property
+    def goal(self):
+        """Return current goal of environment."""
+        if hasattr(self.env, "goal"):
+            return self.env.goal
+        return None
+
+    @property
+    def state(self):
+        """See `AbstractEnvironment.state'."""
+        if hasattr(self.env, "state"):
+            return self.env.state
+        elif hasattr(self.env, "s"):
+            return self.env.s
+        elif hasattr(self.env, "_get_obs"):
+            return getattr(self.env, "_get_obs")()
+        else:
+            raise NotImplementedError("Strange state")
+
+    @state.setter
+    def state(self, value):
+        if hasattr(self.env, "state"):
+            self.env.state = value
+        elif hasattr(self.env, "s"):
+            self.env.s = value
+        elif hasattr(self.env, "set_state"):
+            self.env.set_state(
+                value[: len(self.env.sim.data.qpos)],
+                value[len(self.env.sim.data.qpos) :],
+            )
+        else:
+            raise NotImplementedError("Strange state")
+
+    @property
+    def time(self):
+        """See `AbstractEnvironment.time'."""
+        return self._time
+
+    @property
+    def name(self):
+        """Return class name."""
+        return self.env_name
 
 
 def run_on_parameteric_env(steps_per_task, tasks, wrapper, easy_task):
     random_teacher = RandomTeacher(None, wrapper)
 
+
     ref_env = wrapper.create_env(easy_task)
-    dynamical_model = HallucinatedModel.default(ref_env, beta=1.0)
+    wrapped_env = SillyEnvironmentWrapper(ref_env)
+    dynamical_model = HallucinatedModel.default(wrapped_env, beta=1.0)
+    wrapped_env.add_wrapper(HallucinationWrapper)
 
     student = MPCAgent(
         mpc_policy=MPCPolicy(mpc_solver=CEMShooting(dynamical_model=dynamical_model,
